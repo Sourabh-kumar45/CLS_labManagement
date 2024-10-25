12b4e88ef15f8194615d2f775cc2bd0e08378069
Sourabh-kumar45 <sourabhindia45@gmail.com>
2024-10-25 14:03:58 +0530

    created studentInfo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,15 +4,21 @@ import { useState } from 'react'
 import './App.css'
 import DashBoard from './components/DashBoard'
 import Login from './components/Login'
+import Footer from './components/Footer'
+import StudentInfo from './components/StudentInfo'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    Hello world
+    {/* Hello world
       <DashBoard></DashBoard>
       <Login></Login>
+      <br /><br /><br />
+      <Footer></Footer> */}
+      <StudentInfo></StudentInfo>
     </>
   )
 }
