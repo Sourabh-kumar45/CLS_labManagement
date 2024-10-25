60a5eca8afde5b21c77db41d468ccbb38bb0eb25
BitWiseNexus <adityarehpade1@gmail.com>
2024-10-25 14:03:09 +0530

    created navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,15 +4,16 @@ import { useState } from 'react'
 import './App.css'
 import DashBoard from './components/DashBoard'
 import Login from './components/Login'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    Hello world
+      <Navbar></Navbar>
       <DashBoard></DashBoard>
-      <Login></Login>
+      {/* <Login></Login> */}
     </>
   )
 }
