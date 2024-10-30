47ba3e0efb198b90e5f57515c19c4133d8728c2e
Sourabh-kumar45 <sourabhindia45@gmail.com>
2024-10-30 18:59:55 +0530

    added footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className=''>
-        this is a basic footer
-    </div>
+    <>
+      <div className="border-t border-gray-300 my-1"></div>
+      <footer className="flex flex-wrap justify-around py-2 bg-white">
+        <p className="text-gray-500">All copyrights reserved &copy; </p>
+        <span className="text-gray-500">
+          Operated and managed by students of IIT Bhilai with <i className="fa-regular fa-heart"></i>
+        </span>
+      </footer>
+    </>
   )
 }
 
