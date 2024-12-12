93c9314de0e4178a7439cec5202f19dcdfa82821
BitWiseNexus <adityarehpade1@gmail.com>
2024-12-12 12:04:10 +0530

    student form ban gaya hai

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,25 +1,54 @@
 import React, { useState } from 'react';
 import {useNavigate} from 'react-router-dom'
 import axios from 'axios'
+import AlertBox from './AlertBox';
 
 const Register = () => {
 
   const [name, setName] = useState();
   const [email,setEmail] = useState();
   const [password,setPassword] = useState();
+  const [confirmPwd,setConfirmPwd] = useState();
   const navigate = useNavigate()
 
-    // we are using the axios to post the data
+  //we have to modify this later to make dynamic. 
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [errorTimestamp, setErrorTimestamp] = useState(null); // using timestamps for multiple rerendering.
 
+   // we are using the axios to post the data
   const handleSubmit = (e) =>{
-    e.preventDefault()
-    axios.post('http://localhost:3000/register',{name,email,password})
-    .then(result => console.log(result))
-    navigate('/login')
-    .catch(err=> console.log(err))
+    if(password === confirmPwd){
+      e.preventDefault()
+      axios.post('http://localhost:3000/register',{name,email,password})
+      .then((result) => {
+        console.log(result);
+        navigate('/login',{ state: { message: "Sign up succesful", type: "success" } });
+      })
+      .catch((err) => {
+        console.error(err);
+        setErrorMessage({ errorType: 'error', message: 'Registration failed' });
+        setErrorTimestamp(Date.now()); // Update timestamp
+      });
+      
+    }
+   else{
+    setErrorMessage({
+      errorType: 'error',
+      message: 'Passwords do not match. Please try again.',
+    });
+    setErrorTimestamp(Date.now()); // Update timestamp
+    console.log('Passwords do not match. Please enter the password again.');
+   }
   }
   return (
     <div>
+      {errorMessage && (
+        <AlertBox message={errorMessage.message} type={errorMessage.errorType} key={errorTimestamp} />
+        )}
+      <div className="flex justify-center items-center min-h-screen bg-zinc-100"  >
+          {/* key forces re render  key={errorMessage} */}
+        <div className="w-full max-w-md bg-white p-8 rounded-lg shadow-lg">
+
             <h2 className="text-2xl font-bold text-center text-blue-600 mb-6">Sign Up</h2>
             <div className="mb-4">
               <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="login-email">
@@ -57,8 +86,8 @@ const Register = () => {
                 onChange={(e) => setPassword(e.target.value)}
               />
             </div>
-            {/* i would implement confirm password later */}
-            {/* <div className="mb-4">
+            
+            <div className="mb-4">
               <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="signup-confirm-password">
                 Confirm Password
               </label>
@@ -67,8 +96,9 @@ const Register = () => {
                 id="signup-confirm-password"
                 placeholder="Confirm your password"
                 className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
+                onChange={(e) => setConfirmPwd(e.target.value)}
               />
-            </div> */}
+            </div>
             <button type = 'submit'
               className="w-full bg-blue-600 text-white font-bold py-2 px-4 rounded-lg hover:bg-blue-700 focus:outline-none focus:bg-blue-700"
               onClick={handleSubmit}
@@ -76,6 +106,8 @@ const Register = () => {
               Sign Up
             </button>
           </div>
+      </div>
+    </div>
   )
 }
 
